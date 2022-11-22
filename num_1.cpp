fc890ead8078dc15338843be30ead13778abb938
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-11-22 12:37:00 +0400

    Create num_1.cpp

---
+++ num_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+int x,y;
+cin >> x >> y;
+cout << setprecision(4) << ( (double(1 + sin(x + y)sin(x+y))) / (2 + abs(x - 2 * x / (1 + xx * y*y)))) + x;
+return 0;
+}
