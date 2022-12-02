83ef5f3bde21cbdd8f375c442395b8b1b32f12e2
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-02 13:26:13 +0400

    Update num_11.cpp

--- num_11.cpp
+++ num_11.cpp
@@ -1,19 +1,40 @@
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 
+
 int main()
 {
-	float a1 =0, a2, n, i;
+	float n, i;
+	int a1, a2, a3;
 
 	cin >> n;
-	cout << a1 << endl;
-	for (i = 1; i < n; i += 0.5) {
-		a2 = a1 + 3;
-		cout << a2 << endl;
-		a1 = a2;
+	a1 = 0;
+	for (i = 1; i <= n; i++) {
+		if (i == 1) {
+			cout << i << ' ' << a1 << endl;
+			
+		}
+
+		if (i > 2) {
+			a2 = a1 + 3;
+			a3 = 2 * a2;
+			cout << i << ' ' << a3 << endl;
+			a1 = a2;
+			a2 = a3;
+		}
+
+		if (i == 2) {
+			a2 = a1 + 3;
+			a3 = 2 * a2;
+			cout << i << ' ' << a2 << endl;
+			cout << i + 1 << ' ' << a3 << endl;
+			a1 = a2;
+			a2 = a3;
+			i++;
+		}
 	}
-	return 0;
-} 
+}
