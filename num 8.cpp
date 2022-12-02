7bbeaf5785e24a3a7bdfb764967bfa7360cd2250
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-02 12:28:00 +0400

    Update num 8.cpp

--- num 8.cpp
+++ num 8.cpp
@@ -1,4 +1,27 @@
+
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void f(int &a) {
+	cout << ((a % 10) * 10) + a / 10;
+}
+
+
+int main()
+{
+	int a;
+	cin >> a;
+	f(a);
+	return 0;
+}
+
+// сверху новое, снизу старое
+
+
+
+/*#include <iostream>
 #include <cmath>
 
 using namespace std;
