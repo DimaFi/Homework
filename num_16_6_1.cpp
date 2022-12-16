22d59eb7eee62277426364c5c4802ad91a141e35
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-16 12:17:02 +0400

    Update num_16_6_1.cpp

--- num_16_6_1.cpp
+++ num_16_6_1.cpp
@@ -45,13 +45,39 @@ void fm(int a, int b) {
 		}
 		k[i] = i;
 		l[i] = c;
- 		cout << i << ' ' << c << endl;
+	}
+	int max = -10000;
+	for (i = a; i <= b; i++) {
+		if (l[i] > max) {
+			max = l[i];
+		}
+	}
+	for (i = a; i <= b; i++) {
+		if (l[i] == max) {
+			cout << k[i] << ' ' << l[i] << endl;
+		}
 	}
 }
 
+void f(int a) {
+	int c, i, j;
+	for (i = a; ; i++) {
+		c = 0;
+		for (j = 1; j <= i; j++) {
+			if (i % j == 0) {
+				c++;
+			}
+		}
+		if (c == n) {
+			cout << i << ' ' << c << endl;
+		}
+	}
+}
+
+
 int main()
 {
-	int n, a, b;
+	int n, a, b, aa;
 
 	cout << "Enter [a b]" << endl;
 	cin >> a >> b;
@@ -59,7 +85,11 @@ int main()
 
 	cout << "Enter N del" << endl;
 	cin >> n;
-	fm(a, b);
+	fm(a, b,n);
+	
+	cout << "A - enter " << endl;
+	cin >> aa;
+	f(aa);
 
 
 }
