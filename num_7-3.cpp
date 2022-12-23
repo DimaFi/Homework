def31db08c4e66c8096d48805058a2be13f87fee
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-23 14:03:32 +0400

    Update num_7-3.cpp

--- num_7-3.cpp
+++ num_7-3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int i, ss,c;
+    int i, ss, c;
     string s, s1;
     char s2;
 
@@ -19,16 +19,19 @@ int main()
     bool fl = 1;
 
     for (i = 0; i < s.length(); i++) {
-        if (s[i]==' '|| i+1 == s.length()) {
+        if (s[i] == s2) {
+            fl = 1;
+        }
+        if (s[i] == ' ' || i + 1 == s.length()) {
             if (fl) {
-                cout << s.substr(c, i - c) << ' ';
+                cout << s.substr(c, i - c);
+                if (i - c == 1) {
+                    cout << s.substr(c+1, i - c+1);
+                }
             }
             c = i;
             fl = false;
 
-        }
-        if (s[i] == s2) {
-            fl = 1;
         }
     }
 
