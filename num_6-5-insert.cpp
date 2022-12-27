0ff67a6166fdd4cfe514086104339a91b7f0ef8b
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-27 16:34:29 +0400

    Update num_6-5-insert.cpp

--- num_6-5-insert.cpp
+++ num_6-5-insert.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 
-
 using namespace std;
 
 
 int main()
 {
-    int n, i, j, c;
-
+    int n,i,j;
     cin >> n;
-
-    int* p = new int[2*n];
-    c = 0;
+    int* a = new int[2*n];
+    
     for (i = 0; i < n; i++) {
-
-        cin >> p[c];
-        if (p[c] % 2 == 0) {
-            p[c + 1] = p[c];
-            p[c] = -22;
-            c+=2;
-        }
-        else {
-            c++;
+        cin >> a[i];
+    }
+    int m;
+    m = n;
+    for (i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            for (j = n; j > i; j--) {
+                a[j] = a[j - 1];
+            }
+            n++;
+            i++;
+            a[i] = -5;
         }
     }
 
-    for (i = 0; i < c; i++) {
-        cout << p[i] << ' ';
+    for (i = 0; i < n; i++) {
+        cout << a[i] << ' ';
     }
 }
