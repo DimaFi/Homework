7ebe165fc0716611461ab4480864b6f5878c4f3f
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-04-10 22:44:56 +0400

    Update 14.03_03.cpp

--- Seminar/Second-Sem/14.03_03.cpp
+++ Seminar/Second-Sem/14.03_03.cpp
@@ -1,19 +1,8 @@
-#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
-#include <cmath>
-#include <set>
-#include <map>
-#include <vector>
-#include <cctype>
-#include <string>
-#include <queue>
-#include <deque>
-#include <ctime>
-#include <Windows.h>
 #include <fstream>
-#include <array>
 
 
 using namespace std;
@@ -34,10 +23,6 @@ template <typename x> x fg(int n, x t) {
 }
 
 int main(){
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);;
     int n, b, m;
     //int a;
     double a;
@@ -47,5 +32,4 @@ int main(){
     *h = fg(n, a);
     arrcin(h, n);
     arrout(h, n);
-    return 0;
 }
