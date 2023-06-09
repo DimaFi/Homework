32024d23a9e35cf9c67ce6488edb1cf99756b83f
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-06-09 11:09:52 +0400

    function_2-4.cpp

--- Practice/Sec-sem/function_2-4.cpp
+++ Practice/Sec-sem/function_2-4.cpp
@@ -3,19 +3,19 @@
 #include <iostream>
 using namespace std;
 
-int count(int N) {
+int count(int n) {
     int count = 0;
-    for (int i = 1; i <= N; i++) {
-        if (N % i == 0) {
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
             count++;
         }
     }
     return count;
 }
 
-void printNumber(int A) {
-    int countA = count(A);
-    int nextNumber = A + 1;
+void printNumber(int a) {
+    int countA = count(a);
+    int nextNumber = a + 1;
     while (count(nextNumber) != countA) {
         nextNumber++;
     }
