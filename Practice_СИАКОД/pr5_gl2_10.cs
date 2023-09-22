bf5ff67b9eced010c8a545a09ecd07ee296a5de5
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-09-22 09:20:05 +0400

    Update pr5_gl2_10.cs

--- Practice_СИАКОД/pr5_gl2_10.cs
+++ Practice_СИАКОД/pr5_gl2_10.cs
@@ -21,42 +21,45 @@ class Program
 
     static void Main()
     {
-        // Примеры использования метода
-
         // a) Вывести значения x в n-ной степени для каждого x на отрезке [a, b] с шагом h
+
         int a = -1, b = 3, h = 1;
         for (int x = a; x <= b; x += h)
         {
-            int n = (x - a) / h + 1; // Порядковый номер числа x
+            int n = (x - a) / h;
             long result = FastPower(x, n);
             Console.WriteLine($"{x} в степени {n} = {result}");
         }
 
         // b) Найти сумму всех чисел x^n
+
         long sum = 0;
         for (int x = a; x <= b; x += h)
         {
-            int n = (x - a) / h + 1; // Порядковый номер числа x
+            int n = (x - a) / h;
             long result = FastPower(x, n);
             sum += result;
         }
-        Console.WriteLine($"Сумма всех чисел x^n: {sum}");
+        Console.WriteLine($"Сумма: {sum}");
 
         // c) Найти все тройки различных целых чисел x, y, z таких, что x^2 + y^2 = z^2
+
         int start = 2, end = 6;
         for (int x = start; x <= end; x++)
         {
             for (int y = x + 1; y <= end; y++)
             {
                 double z = Math.Sqrt(x * x + y * y);
-                if (z == (int)z)
+                if (z != (int)z)
                 {
-                    Console.WriteLine($"Тройка {x}, {y}, {(int)z}");
+                    continue;
                 }
+                Console.WriteLine($"Тройка {x}, {y}, {(int)z}");
             }
         }
 
         // d) Найти такое число n, для которого выполняется неравенство 2^(n-1) ≤ a < 2^n
+
         long aValue = 10;
         int nValue = 0;
         while (Math.Pow(2, nValue - 1) <= aValue && aValue < Math.Pow(2, nValue))
