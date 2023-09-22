b510a4b09b28ca315b0819ff4cea7b1f49bac839
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-09-22 10:11:40 +0400

    Update pr5_gl4_6.cs

--- Practice_СИАКОД/pr5_gl4_6.cs
+++ Practice_СИАКОД/pr5_gl4_6.cs
@@ -6,22 +6,22 @@ class Program
     {
         if (number > 0)
         {
-            PrintDigits(number / 10); // Рекурсивно вызываем для оставшихся разрядов числа
-            int digit = number % 10; // Получаем последнюю цифру числа
-            Console.Write(digit + " "); // Выводим цифру на экран
+            PrintDigits(number / 10);
+            int digit = number % 10;
+            Console.Write(digit + " ");
         }
     }
 
     static void Main()
     {
-        int A = 123; // Начальное значение A
-        int B = 135; // Конечное значение B
+        int A = 123;
+        int B = 135;
 
         for (int i = A; i <= B; i++)
         {
             Console.Write($"Цифры числа {i} в прямом порядке: ");
             PrintDigits(i);
-            Console.WriteLine(); // Переход на новую строку для следующего числа
+            Console.WriteLine();
         }
     }
 }
