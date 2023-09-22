8cdcad9e790415673eb00677c98199a96a845bdd
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-09-22 10:11:08 +0400

    Update pr5_gl3_6.cs

--- Practice_СИАКОД/pr5_gl3_6.cs
+++ Practice_СИАКОД/pr5_gl3_6.cs
@@ -2,7 +2,7 @@ using System;
 
 class Program
 {
-    static int CalculateCombinations(int n, int m)
+    static int Combinat(int n, int m)
     {
         if (m == 0 || m == n)
             return 1;
@@ -10,7 +10,7 @@ class Program
         if (m == 1)
             return n;
 
-        return CalculateCombinations(n - 1, m) + CalculateCombinations(n - 1, m - 1);
+        return Combinat(n - 1, m) + Combinat(n - 1, m - 1);
     }
 
     static void Main()
@@ -18,7 +18,7 @@ class Program
         int n = 5;
         int m = 2;
 
-        int result = CalculateCombinations(n, m);
+        int result = Combinat(n, m);
         Console.WriteLine($"C({n}, {m}) = {result}");
     }
 }
