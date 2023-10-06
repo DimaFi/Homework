4bd98ad608f6d45018e5bc2ac78638112cb68055
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-10-06 11:36:16 +0400

    Update pr7_gl6_6.cs

--- Practice_СИАКОД/pr7_gl6_6.cs
+++ Practice_СИАКОД/pr7_gl6_6.cs
@@ -2,44 +2,64 @@ using System;
 
 class Program
 {
+
+    static bool DelRow(ref int[] Array)
+    {
+        for (int i = 0; i < Array.Length; i++)
+        {
+                if (Array[i] % 2 == 0)
+                {
+                    return true;
+                }
+        }
+        return false;
+    }
     static void Main()
     {
+
+        int n = 4;
         int[][] Array = new int[][]
         {
             new int[] { 1, 3, 5, 7 },
             new int[] { 2, 4, 6, 8 },
             new int[] { 11, 13, 15, 17 },
-            new int[] { 10, 20, 30, 40 },
+            new int[] { 11, 13, 15, 17 },
         };
 
-
-        int[][] NewArray = new int[Array.Length][];
         int RowCount = 0;
 
-        for (int i = 0; i < Array.Length; i++)
+        for (int i = 0; i + RowCount < n; i++)
         {
-            bool hasEven = false;
-
-            foreach (int element in Array[i])
+            if (DelRow(ref Array[i + RowCount]) == false)
             {
-                if (element % 2 == 0)
-                {
-                    hasEven = true;
-                    break;
-                }
-            }
-
-            if (hasEven)
-            {
-                NewArray[RowCount] = Array[i];
                 RowCount++;
+                if (RowCount + i != n)
+                {
+                    Array[i] = Array[i + RowCount];
+                }
+                i--;
+            }
+            else
+            {
+                Array[i] = Array[i + RowCount];
             }
         }
 
-        Console.WriteLine("Result:");
-        for (int i = 0; i < RowCount;  i++)
+        if (RowCount != n)
         {
-            Console.WriteLine(string.Join(", ", NewArray[i]));
+            for (int i = 0; i < n - RowCount; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{Array[i][j]} ");
+                }
+                Console.WriteLine();
+            }
         }
+        else
+        {
+            Console.WriteLine("Wasted");
+        }
+
     }
 }
