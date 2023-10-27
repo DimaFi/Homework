7fc16c9607d73a42cb0ae3dd2bb4dc29baa11d7e
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-10-27 10:45:10 +0400

    Update pr9_gl2_16.cs

--- Practice_СИАКОД/pr9_gl2_16.cs
+++ Practice_СИАКОД/pr9_gl2_16.cs
@@ -8,27 +8,13 @@ class Program
         string inputFile = "input.txt";
         string outputFile = "output.txt";
 
-        if (!File.Exists(inputFile))
-        {
-            Console.WriteLine("Исходный файл не найден.");
-            return;
-        }
-
-
         string[] lines = File.ReadAllLines(inputFile);
         double[] numbers = new double[lines.Length];
 
+
         for (int i = 0; i < lines.Length; i++)
         {
-            if (double.TryParse(lines[i], out double number))
-            {
-                numbers[i] = number;
-            }
-            else
-            {
-                Console.WriteLine($"Ошибка чтения числа в строке {i + 1}: {lines[i]}");
-                return;
-            }
+            numbers[i] = double.Parse(lines[i]);
         }
 
         using (StreamWriter writer = new StreamWriter(outputFile))
@@ -39,4 +25,14 @@ class Program
                 writer.WriteLine(average);
             }
         }
+    }
 }
+
+/*
+10
+15
+20
+5
+30
+25
+*/
