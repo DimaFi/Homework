47742b1330162a16807358bd355197a150b07344
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-11-03 00:56:44 +0400

    Create pr14_gl2_6.cs

---
+++ Practice_СИАКОД/pr14_gl2_6.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    struct Product : IComparable<Product>
+    {
+        public string productType;
+        public double price;
+        public string variety;
+        public int quantity;
+
+        public Product(string productType, double price, string variety, int quantity)
+        {
+            this.productType = productType;
+            this.price = price;
+            this.variety = variety;
+            this.quantity = quantity;
+        }
+
+        public int CompareTo(Product other)
+        {
+            return this.quantity.CompareTo(other.quantity);
+        }
+    }
+
+    static void Main()
+    {
+        StreamReader inputFile = new StreamReader("input.txt");
+        List<Product> inventory = new List<Product>();
+        int minQuantity = 10; // Замените эту величину на вашу заданную минимальную величину
+
+        while (!inputFile.EndOfStream)
+        {
+            string productType = inputFile.ReadLine();
+            double price = double.Parse(inputFile.ReadLine());
+            string variety = inputFile.ReadLine();
+            int quantity = int.Parse(inputFile.ReadLine());
+
+            Product product = new Product(productType, price, variety, quantity);
+            inventory.Add(product);
+        }
+        inputFile.Close();
+
+        List<Product> filteredInventory = new List<Product>();
+        foreach (Product product in inventory)
+        {
+            if (product.quantity < minQuantity)
+            {
+                filteredInventory.Add(product);
+            }
+        }
+
+        filteredInventory.Sort();
+
+        StreamWriter outputFile = new StreamWriter("output.txt");
+        foreach (Product product in filteredInventory)
+        {
+            outputFile.WriteLine($"{product.productType} {product.price} {product.variety} {product.quantity}");
+        }
+        outputFile.Close();
+    }
+}
