30b81665af84f8bd82c385e2f4e0f42919650885
DimaFi <53652727+DimaFi@users.noreply.github.com>
2023-12-22 11:51:07 +0400

    Update pr17_main_6.cs

--- Practice_СИАКОД/pr17_main_6.cs
+++ Practice_СИАКОД/pr17_main_6.cs
@@ -8,7 +8,7 @@ using System.Data;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.X86;
-using System.Diagnostics.Metrics;
+//using System.Diagnostics.MetricS;
 using System.Reflection;
 using System.Net.Http.Headers;
 using System.ComponentModel.DataAnnotations;
@@ -23,38 +23,85 @@ class Program
         Console.SetOut(cout);
         cout.AutoFlush = true;
         Console.SetIn(cin);
-
+        int[,] x = { { 1, 2 } , { 3, 4 } };
         // Создание объектов Matric
-        Matric a = new Matric();
-        Matric b = new Matric();
+        Matric a = new Matric(x);
+        Console.WriteLine(a);
+        //Matric b = new Matric();
+        a--;
+        Console.WriteLine(a);
+        a++;
+        Console.WriteLine(a);
 
+        int sum = a.SumOfColumns(0); // Вычисление суммы элементов первого столбца матрицы a
+        Console.WriteLine(sum);
 
-        a = Matric.EnteringElements(cin);
-
-        if (a) // Проверка на квардрат
+        if (a)
         {
-            Console.WriteLine("Square"); 
+            Console.WriteLine("Square");
+        }
+        else
+        {
+            Console.WriteLine("NonSquare");
         }
 
-        a--;
         Console.WriteLine();
         Console.WriteLine(a);
 
-        b = a++;
         a[0, 0] = -1;
-
-        int sum = a.SumOfColumns(0); // Вычисление суммы элементов первого столбца матрицы a
 
         Console.WriteLine();
         a[0, 9] = 999;
         Console.WriteLine(a);
         Console.WriteLine();
-        Console.WriteLine(b);
         int[,] z = a;
         Matric t = new Matric();
         t = z;
 
+
         Console.WriteLine(t.QuantitiOfZeroElements);
         Console.WriteLine(z.Length);
+
+        Matric b = new Matric();
+
+        b = Matric.EnteringElements(cin);
+        b++;
+        Console.WriteLine(b);
+        b--;
+        Console.WriteLine(b);
+
+
+
+
+
+
+
+        //a = Matric.EnteringElements(cin);
+
+        //if (a) // Проверка на квардрат
+        //{
+        //    Console.WriteLine("Square"); 
+        //}
+
+        //a--;
+        //Console.WriteLine();
+        //Console.WriteLine(a);
+
+        //b = a++;
+        //a[0, 0] = -1;
+
+        //int sum = a.SumOfColumns(0); // Вычисление суммы элементов первого столбца матрицы a
+
+        //Console.WriteLine();
+        //a[0, 9] = 999;
+        //Console.WriteLine(a);
+        //Console.WriteLine();
+        //Console.WriteLine(b);
+        //int[,] z = a;
+        //Matric t = new Matric();
+        //t = z;
+
+        //Console.WriteLine(t.QuantitiOfZeroElements);
+        //Console.WriteLine(z.Length);
     }
 }
